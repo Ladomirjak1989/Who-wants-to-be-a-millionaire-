dc45b688ae4e2ccc371db187dda904ce097fafea
Bettina <bettike18989@gmail.com>
2024-02-15 12:02:59 +0100

    RULES

---
+++ script/rules.js
@@ -0,0 +1,11 @@
+const parent = document.querySelector('#rules')
+parent.insertAdjacentHTML('beforeend', 
+`<h2>${rules.title}</h2> 
+<p>${rules.paragraph1}</p> 
+<p>${rules.paragraph2}</p>
+<p>${rules.paragraph3}</p> 
+<p>${rules.paragraph4}</p> 
+<p>${rules.paragraph5}</p> 
+<ul>${rules.lifelines.map(item => {
+    return `<li>${item}</li>`
+}).join('')}</ul>`)
